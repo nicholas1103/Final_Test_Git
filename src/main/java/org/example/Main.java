d6301d79d54abc5d50525106d9d7f14e9da92134
Tan_Kiet <kietpro711@gmail.com>
2024-03-27 09:09:20 +0700

    kl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         for(int i = 0; i<10 ; i++){
-            System.out.println("Nguyen tan kiet");
+            System.out.println("Nguyen tan kiet 711");
         }
     }
 }
